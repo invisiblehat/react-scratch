9b5779bb3b184872e2e276d04690a26bc8a5611f
Joel <joelblackbeatty@gmail.com>
2018-03-06 18:11:42 +0000

    added .env files, error handling, parsers and http request logger

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,12 @@ const router = express.Router();
 
 /* GET home page. */
 // handles any requests made to the application by responding with a single view:
-router.get('*', (req, res) => {
+
+router.get('/about', (req, res) => {
+  res.render('about');
+});
+
+router.get('/', (req, res) => {
   res.render('index');
 });
 
