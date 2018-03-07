b0d0782e18bce286ae6cc139cc5ca8d9c8f19af2
Joel <joelblackbeatty@gmail.com>
2018-03-07 19:12:53 +0000

    updated webpack.config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 /**
  * src/index.js
  */
-
+import styles from './styles.scss';
 import { add, subtract } from './module';
 
 const resultA = add(2, 3);
 const resultB = subtract(5, 1);
 
 console.log(resultA, resultB);
+console.log(styles);
+console.log(styles.globalClass);
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+/**
+ * Webpack configuration for development
+ */
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: path.join(process.cwd(), 'src/index'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(process.cwd(), 'public', 'js'),
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        use: 'eslint-loader',
+      },
+      {
+        test: /\.js?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(s)?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: 'svg-inline-loader',
+      },
+      {
+        test: /\.(jpe?g|png|gif|ico)$/i,
+        use: 'file-loader',
+      },
+    ],
+  },
+  target: 'web',
+};
